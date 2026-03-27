936d5314e6de94e2cde968b0851a1bcb6e9525ee
vjm0186 <vmcelwee@att.net>
2026-03-27 11:13:00 -0400

    Populate Main.js with ready function

---
+++ EndOfLifePlanner/JS/Main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    /* 
+        
+        Begin Revision Comments 
+    
+        End Revision Comments 
+    */
+    
+    /* Begin global varables */
+    
+    /* End global varables */
+    
+    }); /* $(document).ready(function() */
