862853aca499e8cec071ad19a8dfcbe927132f06
vjm0186 <vmcelwee@att.net>
2026-04-04 15:44:26 -0400

    Added Documents

--- EndOfLifePlanner/JS/Main.js
+++ EndOfLifePlanner/JS/Main.js
@@ -8,11 +8,13 @@
 
 const button01 = document.getElementById("btn01");   
 const button02 = document.getElementById("btn02");  
-const button03 = document.getElementById("btn03");  
+const button03 = document.getElementById("btn03"); 
+const button04 = document.getElementById("btn04");   
 
 const AboutText = document.getElementById("AboutText");  
 const PrepaidFuneral = document.getElementById("PrepaidFuneral"); 
 const Entombment = document.getElementById("Entombment"); 
+const Documents = document.getElementById("Documents"); 
 
 /* End global varables */
 
@@ -28,6 +30,10 @@ button03.addEventListener("focus", EntombmentDisplay);
 button03.addEventListener("click", EntombmentDisplay); 
 button03.addEventListener("blur", EntombmentNoDisplay);
 
+button04.addEventListener("focus", DocumentsDisplay);
+button04.addEventListener("click", DocumentsDisplay); 
+button04.addEventListener("blur", DocumentsNoDisplay);
+
 
 /* Begin About Functions */
 function aboutDisplay() {
@@ -35,12 +41,14 @@ function aboutDisplay() {
   PrepaidFuneral.style.visibility = "hidden";
   PrepaidFuneral.style.visibility = "hidden";
   Entombment.style.visibility = "hidden";
+  Documents.style.visibility = "hidden";
 };
 
 function aboutNoDisplay() {
   AboutText.style.visibility = "hidden";
   PrepaidFuneral.style.visibility = "hidden";
   Entombment.style.visibility = "hidden";
+  Documents.style.visibility = "hidden";
 };
 /* End About Functions */
 
@@ -49,12 +57,14 @@ function PrepaidFuneralDisplay() {
   AboutText.style.visibility = "hidden";
   PrepaidFuneral.style.visibility = "visible";
   Entombment.style.visibility = "hidden";
+  Documents.style.visibility = "hidden";
 };
 
 function PrepaidFuneralNoDisplay() {
   AboutText.style.visibility = "hidden";
   PrepaidFuneral.style.visibility = "hidden";
   Entombment.style.visibility = "hidden";
+  Documents.style.visibility = "hidden";
 };
 /* End Prepaid Funeral Functions */
 
@@ -63,11 +73,29 @@ function EntombmentDisplay() {
   AboutText.style.visibility = "hidden";
   PrepaidFuneral.style.visibility = "hidden";
   Entombment.style.visibility = "visible"; 
+  Documents.style.visibility = "hidden";
 };
 
 function EntombmentNoDisplay() {
   AboutText.style.visibility = "hidden";
   PrepaidFuneral.style.visibility = "hidden";
   Entombment.style.visibility = "hidden";
+  Documents.style.visibility = "hidden";
+};
+/* End Entombment Functions */
+
+/* Begin DocumentsFunctions */
+function DocumentsDisplay() {
+  AboutText.style.visibility = "hidden";
+  PrepaidFuneral.style.visibility = "hidden";
+  Entombment.style.visibility = "hidden"; 
+  Documents.style.visibility = "visible";
+};
+
+function DocumentsNoDisplay() {
+  AboutText.style.visibility = "hidden";
+  PrepaidFuneral.style.visibility = "hidden";
+  Entombment.style.visibility = "hidden";
+  Documents.style.visibility = "hidden";
 };
 /* End Entombment Functions */
